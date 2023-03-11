764d27fc7ea0d80e3ec5212d1ffaf3f9188ea0d7
evgenii2104 <evgenii2104@rambler.ru>
2023-03-11 14:12:10 +0300

    new

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const _ = require('lodash')
+const items = [
+    { name: 'Alex', age: 15},
+    { name: 'Dima', age: 13},
+    { name: 'Alex', age: 16},
+]
+
+console.log(_.uniqBy(items, 'name'))
+console.log('new prodgekt')
